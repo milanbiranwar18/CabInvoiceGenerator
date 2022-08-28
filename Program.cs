cb99ff3e567e6fda9c95f94f8e5cd480560be029
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-28 19:20:30 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CabInvoiceGenerator;
+
+namespace CabInvoicegenrator
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            
+        }
+    }
+}
